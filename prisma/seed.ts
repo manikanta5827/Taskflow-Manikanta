85e8cec6328b357fb931c46066d300707a27ec77
Manikanta Thummuri <thummurimanikanta7@gmail.com>
2026-04-10 18:02:42 +0530

    refactor: update seed script to use async/await instead of promise chaining

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -76,11 +76,11 @@ async function main() {
   console.log('----------------------------------------------------');
 }
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+try {
+  await main();
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+} finally {
+  await prisma.$disconnect();
+}
